01dc59fca360f443ee9a6c861097043d314c258e
Samuel Neuenschwander <samuel.neuenschwander@sib.swiss>
2023-11-19 16:25:27 +0100

    Corrected script: wrong usage of variable in 'if' statement.

--- workflow/scripts/mapping_bowtie2.py
+++ workflow/scripts/mapping_bowtie2.py
@@ -2,7 +2,7 @@
 
 import os
 
-if len(input.fastq) == 2:
+if len(snakemake.input.fastq) == 2:
 	os.system(f'bowtie2 {snakemake.params.bowtie2_params} -p {snakemake.threads} -x {snakemake.input.ref} \
 		  -1 {snakemake.input.fastq[0]} -2 {snakemake.input.fastq[1]} 2> {snakemake.log} | \
 		  samtools view -bS - > {snakemake.output}')
